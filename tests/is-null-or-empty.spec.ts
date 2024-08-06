a0e444e7368b14de773d2c2518d37b9c6f8b906a
burakisik <biburakisik@gmail.com>
2024-08-06 02:13:36 +0200

    Increase code coverage

--- tests/is-null-or-empty.spec.ts
+++ tests/is-null-or-empty.spec.ts
@@ -1,6 +1,14 @@
 import { isNullOrEmpty } from "../src";
 
 describe("isNullOrEmpty", () => {
+  test("should be true when value is null", () => {
+    expect(isNullOrEmpty(null)).toEqual(true);
+  });
+
+  test("should be true when value is undefined", () => {
+    expect(isNullOrEmpty(undefined)).toEqual(true);
+  });
+
   test("should be true when value is empty string", () => {
     expect(isNullOrEmpty("")).toEqual(true);
   });
